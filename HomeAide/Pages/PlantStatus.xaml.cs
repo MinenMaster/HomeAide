a57e2e7456f97602f611d06300bc1dfb919dc437
Dominik Meister <dominik.s.meister@gmail.com>
2024-01-07 22:44:36 +0100

    Base Page done

---
+++ HomeAide/Pages/PlantStatus.xaml.cs
@@ -0,0 +1,49 @@
+namespace HomeAide.Pages;
+
+public partial class PlantStatus : ContentPage
+{
+	public PlantStatus()
+	{
+		InitializeComponent();
+    }
+
+    private async void OnOverviewClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Overview());
+    }
+
+    private async void OnLogbookClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Logbook());
+    }
+
+    private async void OnHistoryClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new History());
+    }
+
+    private async void OnEnergyClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Energy());
+    }
+
+    private async void OnPlantStatusClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new PlantStatus());
+    }
+
+    private async void OnDeveloperToolsClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new DeveloperTools());
+    }
+
+    private async void OnSettingsClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Settings());
+    }
+
+    private async void OnHelpClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Help());
+    }
+}
